5aa92acba3a8a18870e477ad17785119e97eb5b7
alicanozer <alicanozer60@gmail.com>
2019-09-23 15:23:38 -0500

    admin panel fix

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../service/auth.service';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   template: `
     <h1 mat-dialog-title>Registration Successfully</h1>
     <mat-dialog-content>
-      Welcome {{this.authService.getUsername()}}
+      Registered...}
     </mat-dialog-content>
     <mat-dialog-actions>
       <!-- <button mat-button (click)="this.router.navigate(['home']);">Go Home</button>
@@ -19,8 +19,3 @@ export class DialogComponent {
   constructor(public router: Router, public authService: AuthService) { }
 
 }
-
-
-////////////
-/// please put this component in a folder ///
-////////////
